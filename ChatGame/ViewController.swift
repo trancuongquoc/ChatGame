39c881116d8017cafd9a245b46fc773070d73d7a
Cuong <cuong@cuongs-imac.local>
2018-09-05 15:12:23 +0700

    Initial Commit

---
+++ ChatGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatGame
+//
+//  Created by Cuong on 9/5/18.
+//  Copyright © 2018 quoccuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
